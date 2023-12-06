69eec410b8f7ca25318a026a7880735433842295
Cara-Cai <54806132+Cara-Cai@users.noreply.github.com>
2023-12-06 14:31:49 -0500

    Update star.js texture path

--- objects/star.js
+++ objects/star.js
@@ -13,7 +13,8 @@ const starTypes = {
 }
 
 
-const texture = new THREE.TextureLoader().load('../resources/sprite120.png')
+// const texture = new THREE.TextureLoader().load('../resources/sprite120.png')
+const texture = new THREE.TextureLoader().load('https://cara-cai.github.io/Sentimental_Galaxy/resources/sprite120.png')
 const materials = starTypes.colorNeutral.map((color) => new THREE.SpriteMaterial({map: texture, color: color}))
 const deltaTime=10
 
